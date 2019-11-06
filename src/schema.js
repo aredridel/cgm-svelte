c8bd59df3a5414cfdb4432e5c02ce2ff04cb8011
Aria Stewart <aredridel@dinhe.net>
2019-11-05 21:57:56 -0500

    Extend schema to include direction and device

--- src/schema.js
+++ src/schema.js
@@ -1,31 +1,50 @@
-const sgvSchema  ={
+const sgvSchema = {
   title: "stored glucose values",
-  version: 1,
+  version: 2,
   decription: "stored glucose values",
   type: "object",
   properties: {
     ts: {
       type: "number",
-      index: true,
+      index: true
     },
     sgv: {
       type: "number"
+    },
+    device: {
+      type: "string"
+    },
+    direction: {
+      enum: [
+        "NONE",
+        "ExtremeRise",
+        "FastRise",
+        "Rise",
+        "Flat",
+        "Fall",
+        "FastFall",
+        "ExtremeFall",
+        "NOT COMPUTABLE",
+        "RATE OUT OF RANGE"
+      ],
+      default: "NONE"
     }
   },
   required: ["sgv", "ts"]
-}
+};
 
 export default async function configure(dbP) {
-    const db = await dbP;
+  const db = await dbP;
 
-    const sgv = await db.collection({
-        name: 'sgv',
-        schema: sgvSchema,
-        autoMigrate: true,
-        migrationStrategies: {
-          1: doc => doc
-        }
-    });
-    db.collections.sgv.$.subscribe(x => console.log('sgv', x))
-    return db;
+  const sgv = await db.collection({
+    name: "sgv",
+    schema: sgvSchema,
+    autoMigrate: true,
+    migrationStrategies: {
+      1: doc => doc,
+      2: doc => Object.assign({}, doc, { direction: "NONE" })
+    }
+  });
+  db.collections.sgv.$.subscribe(x => console.log("sgv", x));
+  return db;
 }
